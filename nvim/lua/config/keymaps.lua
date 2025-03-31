68f3637ae590395887e70dd523b2a4492463938a
rajneesh.kumar <rajneesh.kumar@innoscripta.com>
2025-04-01 01:13:57 +0530

    fix:add the dev ops nvim config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,108 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- keymaps.lua
+
+-- Override <Space>l to make it do nothing
+
+-- Ensure LazyVim has finished loading
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",  -- This event fires after LazyVim has loaded
+  callback = function()
+    -- Override <Space>l to make it do nothing after LazyVim is loaded
+    vim.keymap.set("n", "<Space>l", "<NOP>", { noremap = true, silent = true, desc = "Disable Lazy UI popup" })
+  end
+})
+
+
+local keymap = vim.keymap -- for conciseness
+
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+-- keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- -- Jumplist
+-- keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- -- Diagnostics
+-- keymap.set("n", "<C-j>", function()
+-- 	vim.diagnostic.goto_next()
+-- end, opts)
+
+-- keymap.set("n", "<leader>r", function()
+-- 	require("craftzdog.hsl").replaceHexWithHSL()
+-- end)
+-- keymap.set("n", "<leader>i", function()
+-- 	require("craftzdog.lsp").toggleInlayHints()
+-- end)
+
+--split
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+--tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode with Esc" })
+
+-- Resize window using Ctrl + Arrow keys
+keymap.set("n", "<C-Left>", "<C-w><", { desc = "Decrease window width" })
+keymap.set("n", "<C-Right>", "<C-w>>", { desc = "Increase window width" })
+keymap.set("n", "<C-Up>", "<C-w>+", { desc = "Increase window height" })
+keymap.set("n", "<C-Down>", "<C-w>-", { desc = "Decrease window height" })
+
+
+keymap.set("n", "<leader>o", ":silent !open .<CR>", { desc = "Open current directory in Finder" })
+
+
+--Open the current file  neo tree
+keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', {})
+
+-- Open in Finder from Neotree
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "neo-tree",
+    callback = function()
+        vim.keymap.set("n", "<leader>o", function()
+            local node = require("neo-tree.sources.filesystem.commands").get_current_node()
+             if node then
+              
+                if node.type == "file" then
+                    vim.fn.system(string.format("open -R '%s'", abs_path))
+                else
+                    vim.fn.system(string.format("open '%s'", abs_path))
+                end
+            else
+                print("No node found under cursor")
+            end
+        end, { buffer = true, desc = "Open in Finder" })
+    end
+})
